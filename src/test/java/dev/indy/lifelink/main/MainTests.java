073af01d29ceb77bfd56b870fd2fa6a9ff65438c
indyy_ <38891612+indyplaygame@users.noreply.github.com>
2025-11-18 14:33:20 +0100

    Initial commit

---
+++ src/test/java/dev/indy/lifelink/main/MainTests.java
@@ -0,0 +1,13 @@
+package dev.indy.lifelink.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
